02bd0f9ff7ea707f72d55886f3ffed2de3b96bb5
Gabriel Rikson <gabrielrikson@outlook.com>
2020-10-08 18:27:04 -0300

    update php

--- php/email.php
+++ php/email.php
@@ -8,8 +8,8 @@
 
         $to = "gabrielrikson@outlook.com";
         $subject = "Contato de Serviço Vidraçaria";
-        $body = "Nome: ".$nome. "\r\n"
-                "Email: ".$email. "\r\n"
+        $body = "Nome: ".$nome. "\r\n".
+                "Email: ".$email. "\r\n".
                 "Mensagem: ".$mensagem;
         $header = "From: gabrielrikson@outlook.com"."\r\n"
                     ."Reply-To:".$email."\r\n"
